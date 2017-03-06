2160374179d9aac74d5c016c96ff6f681fb3bf34
Todd Quessenberry <todd@quessenberry.com>
2017-03-06 15:07:03 -0800

    Started a BeerList component, changed indent to 4

---
+++ src/components/BeerList.vue
@@ -0,0 +1,21 @@
+<template>
+<ul>
+    <li>Beer 1</li>
+    <li>Beer 2</li>
+    <li>Beer 3</li>
+</ul>
+</template>
+
+<script>
+export default {
+    name: 'beer-list',
+    data () {
+        return {}
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import BeerList from '@/components/BeerList'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'BeerList',
+            component: BeerList
+        },
+        {
+            path: '/hello',
+            name: 'Hello',
+            component: Hello
+        }
+    ]
 })
